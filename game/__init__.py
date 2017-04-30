3bbe105e722c0fa6eb3f88dbdbc68f5121b9f613
cshenton <charlieshenton@me.com>
2017-04-30 12:44:25 +1000

    random direction generator as placeholder for model

--- game/__init__.py
+++ game/__init__.py
@@ -1,6 +1,7 @@
 """
 This is a simple web app that plays a game of keep away against the user. Game state is updated at the client by querying the backend api, and the webserver also serves the static html+js at root.
 """
+from random import randint
 from sanic import Sanic
 
 app = Sanic(__name__)
@@ -14,4 +15,39 @@ async def action_handler(request):
     """
     When a user posts game state, the server should respond with the action for the game AI to take.
     """
-    return text('POST request - {}'.format(request.json))
+    body = request.json
+    # Do some stuff
+
+    response = {
+        'direction': random_direction()
+    }
+    return json(response)
+
+def random_direction():
+    """
+    Generates and returns a random cardinal direction as a strings
+    """
+    directions = {
+        1: 'up',
+        2: 'down',
+        3: 'left',
+        4: 'right',
+    }
+    return directions[randint(1,4)]
+
+
+REQUEST = {
+    'hero': {
+        'x': int,
+        'y': int,
+    }
+    'monster': {
+        'x': int,
+        'y': int,
+    }
+    'caught': bool
+}
+
+RESPONSE = {
+    'direction': str # one of 'up' 'down' 'left' 'right'
+}
