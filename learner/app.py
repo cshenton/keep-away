8b969279abddb67014bd9413dd29b9ba94dd4dfb
cshenton <charlieshenton@me.com>
2017-05-02 22:27:49 +1000

    shove model stuff in a class

--- learner/app.py
+++ learner/app.py
@@ -1,34 +1,37 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 
-def init_model():
+class Learner:
     """
-    Layers and compiles the Keras model
+    Responsible for training and persisting the Neural Net.
     """
-    model = Sequential([
-        Dense(32, input_shape=(784,)),
-        Activation('relu'),
-        Dense(10),
-        Activation('softmax'),
-    ])
+    def __init__(self):
+        """
+        Initialise the Keras Neural Net and attach it to the object.
+        """
+        model = Sequential([
+            Dense(32, input_shape=(784,)),
+            Activation('relu'),
+            Dense(10),
+            Activation('softmax'),
+        ])
+        adam = Adam(lr=1e-6)
+        model.compile(loss='mse',optimizer=adam)
+        self.model = model
 
-    adam = Adam(lr=1e-6)
-    model.compile(loss='mse',optimizer=adam)
-    return model
+    def replay(self):
+        """
+        Gets a random sample of remembered data from redis
+        """
+        pass
 
-def replay():
-    """
-    Gets a random sample of remembered data from redis
-    """
-    pass
-
-def update(model):
-    """
-    Trains model on replayed dta
-    """
-    X, y = replay()
-    # Or model.fit_generator(data_generator, steps_per_epoch, epochs)
-    model.train_on_batch(X, y)
+    def update(self):
+        """
+        Trains model on replayed dta
+        """
+        X, y = replay()
+        # Or model.fit_generator(data_generator, steps_per_epoch, epochs)
+        model.train_on_batch(X, y)
 
 def main():
     """
@@ -40,3 +43,6 @@ def main():
     # batch update the model
     # every now and then repersist the graph to redis
     pass
+
+if __name__ == '__main__':
+    main()
